f28668f603f4206e3aeb018f4c6cba2da3bf7db0
hao <1291536750@qq.com>
2019-10-31 07:30:15 +0800

    Finished

--- LeetCode/3. Longest Substring Without Repeating Characters(string).cpp
+++ LeetCode/3. Longest Substring Without Repeating Characters(string).cpp
@@ -1,25 +1,25 @@
+/**
+ * 暴力法，时间比较大 
+ */
 int lengthOfLongestSubstring(char * s){
     int count = 1;
     int index;
     int max = 1;
-    int book[66] = {0};
+    int book[150] = {0}; 
     int lenght = strlen(s);
+    if(!strcmp(s,"")) return 0;
     for(int i=0;i<lenght-1;i++){
         count = 1;
-        memset(book,0,sizeof(int)*66);
-        book[abs(s[i]-'a')] = 1;
+        memset(book,0,sizeof(int)*150);
+        book[s[i]-' '] = 1;
         index = i + 1;
-        while(1){
-            if(!book[abs(s[i]-'a')]){
-                book[abs(s[i]-'a')] = 1;
-                count++;
-            } else break;
-            if(count > max){
-                max = count;
-            }
+        while(!book[s[index]-' ']){
+            book[s[index]-' '] = 1;
             index++;
-            if(index > lenght) break;
+            count++;
+            if(index >= lenght) break;
         }
+        if(count > max) max = count;
     }
     return max;
 }
