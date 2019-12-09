b65aea6861744bcf72318e96117b9d4bf153cf3b
hao <1291536750@qq.com>
2019-12-09 12:57:44 +0800

    ...

---
+++ .leetcode/102.二叉树的层次遍历.cpp
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=cpp
+ *
+ * [102] 二叉树的层次遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (59.90%)
+ * Likes:    327
+ * Dislikes: 0
+ * Total Accepted:    62.3K
+ * Total Submissions: 103.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回其层次遍历结果：
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [9,20],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> record;
+        queue<TreeNode*> q;
+        int count = 0;
+        if(root != NULL){
+            q.push(root);
+            vector[count].push_back(root->val);
+        }
+        while(!q.empty()){
+            TreeNode *tmp = q.top();
+            
+        }
+    }
+};
+// @lc code=end
+
