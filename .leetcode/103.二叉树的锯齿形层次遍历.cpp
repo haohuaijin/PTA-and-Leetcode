d60416e28279ea66662ee3e2c982b8014b427445
hao <1291536750@qq.com>
2019-12-10 14:19:59 +0800

    ...

---
+++ .leetcode/103.二叉树的锯齿形层次遍历.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=cpp
+ *
+ * [103] 二叉树的锯齿形层次遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> record;
+        queue<TreeNode*> q;
+        
+
+    }
+};
+// @lc code=end
+
---
+++ .leetcode/104.二叉树的最大深度.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int Left = 0,Right = 0;
+        if(root == NULL) return 0;
+        Left = maxDepth(root->left) + 1;
+        Right = maxDepth(root->right) + 1;
+        return Left > Right ? Left : Right;
+    }
+};
+// @lc code=end
+
