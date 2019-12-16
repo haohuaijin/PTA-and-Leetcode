31aa7efe8b4e4c231fe36b511aa50d99e4159e72
hao <1291536750@qq.com>
2019-12-16 15:52:51 +0800

    remove leecode file

--- LeetCode/LCP 1. Guess Numbers.cpp
+++
@@ -1,8 +0,0 @@
-int game(int* guess, int guessSize, int* answer, int answerSize){
-    int count = 0;
-    for(int i=0;i<guessSize;i++){
-        if(guess[i] == answer[i]) 
-            count++;
-    }
-    return count;
-}
