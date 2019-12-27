a5882b72dfdf2b6b6fff51e9d29e9e6efea443b1
hao <1291536750@qq.com>
2019-12-27 21:48:12 +0800

    leetcode 337 仔细思考

--- .leetcode/124.二叉树中的最大路径和.cpp
+++ .leetcode/124.二叉树中的最大路径和.cpp
@@ -56,29 +56,45 @@ class Solution {
 public:
     //理解好题目的意思:从任意的节点到任意节点，是用线穿起来的
     //思路：
-    //1.把每一个节点看作最大路径和，计算根加左加右的值
-    //2.每个结点返回的是：根以下最大路径(一条线)
+    //1.把每一个节点看作最大路径和，计算根加左加右的"正值"
+    //2.每个结点返回的是：根以下最大路径(一条线)(左子树或右子树)
     int maxPathSum(TreeNode* root) {
         int Max = -9999;
         Sum(root, Max);
         return Max;
     }
+    /*
+    //优化前, 时间快, 内存消耗少
     int Sum(TreeNode *root,int &Max){
         int LeftMax=0,RightMax=0;
         int NowMax = root->val;
-        int tmp = root->val;
+        int re = root->val;
         if(root->left != nullptr) LeftMax = Sum(root->left, Max);
         if(root->right != nullptr) RightMax = Sum(root->right, Max);
-
+        //判断以当前结点为根,是不是最大值
         if(LeftMax > 0) NowMax += LeftMax;
         if(RightMax > 0) NowMax += RightMax;
+        if(NowMax > Max) Max = NowMax;
+        //确认返回值
+        int tmp = (LeftMax > RightMax ? LeftMax : RightMax);
+        if(tmp > 0) re += tmp;
 
+        return re;
+    }
+    */
+    //优化后, 代码好看时间慢, 内存消耗多
+    int Sum(TreeNode *root,int &Max){
+        if(root == nullptr) return 0;
+
+        int LeftMax = max(Sum(root->left, Max), 0);
+        int RightMax = max(Sum(root->right, Max), 0);
+
+        int NowMax = root->val + LeftMax + RightMax;
         if(NowMax > Max) Max = NowMax;
 
-        int num = (LeftMax > RightMax ? LeftMax : RightMax);
-        if(num > 0) tmp += num;
+        int re = root->val + max(max(LeftMax, RightMax), 0);
 
-        return tmp;
+        return re;
     }
 };
 // @lc code=end
---
+++ .leetcode/337.打家劫舍-iii.cpp
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=337 lang=cpp
+ *
+ * [337] 打家劫舍 III
+ *
+ * https://leetcode-cn.com/problems/house-robber-iii/description/
+ *
+ * algorithms
+ * Medium (54.96%)
+ * Likes:    210
+ * Dislikes: 0
+ * Total Accepted:    11.7K
+ * Total Submissions: 21.2K
+ * Testcase Example:  '[3,2,3,null,3,null,1]'
+ *
+ * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。
+ * 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+ * 
+ * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [3,2,3,null,3,null,1]
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  2   3
+ * ⁠   \   \ 
+ * ⁠    3   1
+ * 
+ * 输出: 7 
+ * 解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+ * 
+ * 示例 2:
+ * 
+ * 输入: [3,4,5,1,3,null,1]
+ * 
+ * 3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \   \ 
+ * ⁠1   3   1
+ * 
+ * 输出: 9
+ * 解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        vector<int> res = robsum(root);
+        return max(res[0], res[1]);
+    }
+    //仔细思考，递归时有两种情况，要返回两个数字用vector<int>
+    vector<int> robsum(TreeNode *root){
+        vector<int> res(2);
+        if(root == nullptr) return res;
+
+        vector<int> Left = robsum(root->left);
+        vector<int> Right = robsum(root->right);
+        //不包含根结点，最大值是左右节点之和
+        res[0] = max(Left[0], Left[1]) + max(Right[0], Right[1]);
+        //包含根节点，最大值为两个子树不包含根节点的最大值加上根节点的值
+        res[1] = Left[0] + Right[0] + root->val;
+
+        return res;
+    }
+};
+// @lc code=end
+
