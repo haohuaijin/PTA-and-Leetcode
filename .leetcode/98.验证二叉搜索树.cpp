62615d31bc04f7f6ab6b43460b63b00a7b5ff76d
hao <1291536750@qq.com>
2019-12-29 22:11:38 +0800

    leetcode 98 vaild BST

--- .leetcode/98.验证二叉搜索树.cpp
+++ .leetcode/98.验证二叉搜索树.cpp
@@ -59,8 +59,108 @@
  */
 class Solution {
 public:
+    /* 暴力，记录左右子树的节点
     bool isValidBST(TreeNode* root) {
-        
+        bool flag = true;
+        vaild(root, flag);
+        return flag;
+    }
+    vector<int> vaild(TreeNode *root,bool &flag){
+        vector<int> res,Left,Right;
+        if(root == nullptr || flag == false) return res;
+        res.push_back(root->val);
+        Left = vaild(root->left, flag);
+        Right = vaild(root->right, flag);
+        for(auto l : Left){
+            if(l >= root->val){
+                flag = false;
+                return res;
+            }
+            res.push_back(l);
+        }
+        for(auto r : Right){
+            if(r <= root->val){
+                flag = false;
+                return res;
+            }
+            res.push_back(r);
+        }
+        return res;
+    }
+    */
+    /*
+    //递归，理解
+    bool isValidBST(TreeNode* root) {
+        return helper(root,LONG_MIN,LONG_MAX);
+    }
+    bool helper(TreeNode *root,long lower,long upper){
+        if(root == nullptr) return true;
+
+        if(lower >= root->val || upper <= root->val)
+            return false;
+        //给结点设置，上下界
+        if(!helper(root->left, lower, root->val)) return false;
+        if(!helper(root->right, root->val, upper)) return false;
+        return true;
+    }*/
+    /*
+    //迭代法,前序遍历
+    bool isValidBST(TreeNode* root) {
+        if(root == nullptr) return true;
+        stack<TreeNode*> node;
+        stack<long> lower,upper;
+        node.push(root);
+        lower.push(LONG_MIN);
+        upper.push(LONG_MAX);
+        while(!node.empty()){
+            TreeNode *tmp = node.top(); node.pop();
+            long low = lower.top(); lower.pop();
+            long up = upper.top();  upper.pop();
+
+            if(tmp->val <= low || tmp->val >= up) return false;
+
+            if(tmp->left != nullptr){
+                node.push(tmp->left);
+                lower.push(low); upper.push(tmp->val);
+            }
+            if(tmp->right != nullptr){
+                node.push(tmp->right);
+                lower.push(tmp->val); upper.push(up);
+            }
+        }
+        return true;
+    }*//*
+    //中序遍历,迭代,stack
+    bool isValidBST(TreeNode* root) {
+        if(root == nullptr) return true;
+        long num = LONG_MIN;
+        stack<TreeNode*> q;
+        //精简版
+        while(!q.empty() || root != nullptr){
+            while(root != nullptr){
+                q.push(root);
+                root = root->left;
+            }
+            root = q.top(); q.pop();
+            if(num >= root->val) return false;
+            num = root->val;
+            root = root->right;
+        }
+        return true;
+    }*/
+    //递归，中序遍历，升序不重复
+    bool isValidBST(TreeNode* root) {
+        if(root == nullptr) return true;
+        long num = LONG_MIN;
+        bool flag = true;
+        DFS(root, num, flag);
+        return flag;
+    }
+    void DFS(TreeNode *root,long &num,bool &flag){
+        if(root->left != nullptr) DFS(root->left, num, flag);
+        if(num >= root->val) flag = false;
+        num = root->val;
+        if(root->right != nullptr) DFS(root->right, num, flag);
     }
 };
 // @lc code=end
