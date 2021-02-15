dafe2aeb3220c98e4d5fe95e652773f01c2e3751
hhj <1291536750@qq.com>
2021-02-15 22:01:21 +0800

    leetcode 135 435

---
+++ .leetcode/135.分发糖果.cpp
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=135 lang=cpp
+ *
+ * [135] 分发糖果
+ *
+ * https://leetcode-cn.com/problems/candy/description/
+ *
+ * algorithms
+ * Hard (48.17%)
+ * Likes:    472
+ * Dislikes: 0
+ * Total Accepted:    63.1K
+ * Total Submissions: 131K
+ * Testcase Example:  '[1,0,2]'
+ *
+ * 老师想给孩子们分发糖果，有 N 个孩子站成了一条直线，老师会根据每个孩子的表现，预先给他们评分。
+ * 
+ * 你需要按照以下要求，帮助老师给这些孩子分发糖果：
+ * 
+ * 
+ * 每个孩子至少分配到 1 个糖果。
+ * 评分更高的孩子必须比他两侧的邻位孩子获得更多的糖果。
+ * 
+ * 
+ * 那么这样下来，老师至少需要准备多少颗糖果呢？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：[1,0,2]
+ * 输出：5
+ * 解释：你可以分别给这三个孩子分发 2、1、2 颗糖果。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：[1,2,2]
+ * 输出：4
+ * 解释：你可以分别给这三个孩子分发 1、2、1 颗糖果。
+ * ⁠    第三个孩子只得到 1 颗糖果，这已满足上述两个条件。
+ * 
+ */
+
+// @lc code=start
+/*
+// 两次遍历
+class Solution{
+public:
+    int candy(vector<int> &ratings){
+        int len = ratings.size();
+        if(len < 2) return len;
+        vector<int> num(len, 1);
+        for(int i = 1; i < len; ++i){
+            if(ratings[i] > ratings[i-1]) 
+                num[i] = num[i-1] + 1;
+        }
+        for(int i = len-1; i > 0; --i){
+            if(ratings[i] < ratings[i-1])
+                num[i-1] = max(num[i-1], num[i] + 1);
+        }
+        return accumulate(num.begin(), num.end(), 0);
+    }
+};
+*/
+/* 寻找规律 */
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int ret = 1;
+        int inc = 1, dec = 0, pre = 1;
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] >= ratings[i - 1]) {
+                dec = 0;
+                pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                ret += pre;
+                inc = pre;
+            } else {
+                dec++;
+                if (dec == inc) {
+                    dec++;
+                }
+                ret += dec;
+                pre = 1;
+            }
+        }
+        return ret;
+    }
+};
+// @lc code=end
