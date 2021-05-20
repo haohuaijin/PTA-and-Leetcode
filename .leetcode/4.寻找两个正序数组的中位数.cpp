efde17e5b309826f4784cb31ec4af7e561cde211
hhj <1291536750@qq.com>
2021-05-20 21:59:11 +0800

    finish leetcode 4

--- .leetcode/4.寻找两个正序数组的中位数.cpp
+++ .leetcode/4.寻找两个正序数组的中位数.cpp
@@ -74,6 +74,7 @@
  */
 
 // @lc code=start
+/*
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -95,8 +96,41 @@ public:
             return cur;
     }
 };
+*/
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size();
+        int m = nums2.size();
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将奇数和偶数的情况合并，奇数的话，会求两次同样的k；
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + 
+                    getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+    }
+private:
+    int getKth(vector<int>&nums1, int start1, int end1, vector<int> &nums2, int start2, int end2, int k){
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        if(len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if(len1 == 0) return nums2[start2 + k - 1];
+
+        if(k == 1) return min(nums1[start1], nums2[start2]);
+
+        int i = start1 + min(len1, k / 2) - 1;
+        int j = start2 + min(len2, k / 2) - 1;
+
+        if(nums1[i] > nums2[j])
+            return getKth(nums1, start1, end1, nums2, j+1, end2, k - (j - start2 + 1));
+        else 
+            return getKth(nums1, i+1, end1, nums2, start2, end2, k - (i - start1 + 1));
+    }
+};
+
+
 
 // leetcode solutions
+// https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
 /*
 public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int n = nums1.length;
@@ -104,7 +138,8 @@ public double findMedianSortedArrays(int[] nums1, int[] nums2) {
     int left = (n + m + 1) / 2;
     int right = (n + m + 2) / 2;
     //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
-    return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;  
+    return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + 
+            getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;  
 }
     
 private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
