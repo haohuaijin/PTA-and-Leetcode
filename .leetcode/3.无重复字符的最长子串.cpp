d45849f71f37b88e2717d5e5c31eeb89cd2ab9eb
hhj <1291536750@qq.com>
2021-06-03 22:18:16 +0800

    leetcode 3 add lc sloution

--- .leetcode/3.无重复字符的最长子串.cpp
+++ .leetcode/3.无重复字符的最长子串.cpp
@@ -61,6 +61,7 @@
  */
 
 // @lc code=start
+/*
 //滑动窗口
 class Solution {
 public:
@@ -79,18 +80,45 @@ public:
                 while(++p != i && s[p-1] != s[i]){
                     set.erase(s[p-1]);
                 }
-                /*
-                while(left != i){
-                    ++left;
-                    if(s[left-1] != s[i])
-                        set.erase(s[left-1]);
-                    else
-                        break;
-                }
-                */
+                // while(left != i){
+                //     ++left;
+                //     if(s[left-1] != s[i])
+                //         set.erase(s[left-1]);
+                //     else
+                //         break;
+                // }
             }
         }
         return ret;
+    }
+};
+*/
+// leetcode
+// 真正的滑动窗口
+// link: https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // 哈希集合，记录每个字符是否出现过
+        unordered_set<char> occ;
+        int n = s.size();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        // 枚举左指针的位置，初始值隐性地表示为 -1
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.erase(s[i - 1]);
+            }
+            while (rk + 1 < n && !occ.count(s[rk + 1])) {
+                // 不断地移动右指针
+                occ.insert(s[rk + 1]);
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1);
+        }
+        return ans;
     }
 };
 // @lc code=end
